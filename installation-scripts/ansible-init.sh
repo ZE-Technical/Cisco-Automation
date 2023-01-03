1dfcdf6c038d275f09f99b05ece8240fd6bc39ab
ZachThill <zach.thill@comcast.net>
2023-01-03 11:49:42 -0700

    added vars

---
+++ installation-scripts/ansible-init.sh
@@ -0,0 +1,10 @@
+# Download and Initialize Ansible
+#
+apt-get update
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible
+#
+apt-get -y upgrade
+#
